3c0f98cb2f430085c2510b28c8b04d89f0817cb6
ChristopherBarba <30627640+ChristopherBarba@users.noreply.github.com>
2024-02-29 09:39:24 -0600

    Add files via upload

---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+
+const Error = ({children}) => {
+  return (
+
+        <div className='bg-red-800 text-white rounded-md p-3 
+          text-center mb-5 uppercase font-bold'>
+            <p>{children}</p>
+          </div>
+
+  )
+}
+
+export default Error
